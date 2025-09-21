642e44ae0aefc0b0c2dafa3fdca003346195b3af
khai <khaitruong1301@gmail.com>
2025-09-21 14:51:05 +0700

    dp

--- src/app/api/store_app/product/route.tsx
+++ src/app/api/store_app/product/route.tsx
@@ -1,28 +1,10 @@
-import { setCookieCustom } from '@/app/_util/setting';
 import { NextRequest, NextResponse } from 'next/server'
-import { cookies } from "next/headers";
 
-const handleSetcookie = async () => {
-    await setCookieCustom('test_cookie', 'hello_cookie', 7);
-}
-
-
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
     try {
-        const store = await cookies();
-
-        // // set cookie token = abc123, tồn tại 1 giờ
-        // store.set("token", "abc123", {
-        //     httpOnly: true, // không đọc được từ JS client
-        //     path: "/",
-        //     maxAge: 60 * 60, // 1h
-        // });
-
-        // const cookie = store.get('token')?.value || '';
-        // console.log(cookie, 'cookie');
-
+        
         const id = req.nextUrl.searchParams.get('id') || '';
-        console.log('id', id);
+        console.log('id',id);
         const baseUrl = 'https://apistore.cybersoft.edu.vn/api';
         const fetchUrl = id
             ? `${baseUrl}/product/getbyid?id=${id}`
